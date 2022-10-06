2dd4fcbf53fcc859dcfcf895c4b3c24beb662fdc
Alexey <alexeysitiy@gmail.com>
2022-10-06 18:23:13 +0300

    add scroll

--- src/js/Script.js
+++ src/js/Script.js
@@ -42,35 +42,85 @@ $(document).ready(
         toggleButton('.catalog-item__link');
         toggleButton('.catalog-item__back');
 
-    //Modal
-        function closePopUp(){
-            $('.modal__close').on('click', function (){
+        //Modal
+        function closePopUp() {
+            $('.modal__close').on('click', function () {
                 $('.overlay, #consultation-modal, #order-modal, #thanks-modal').fadeOut('fast');
             });
         }
-        function closeCross(){
+
+        function closeCross() {
             $('.overlay, #consultation-modal, #order-modal, #thanks-modal').fadeOut('fast');
         }
 
-        $('[data-modal=consultation]').on('click', function (){
-           $('.overlay, #consultation-modal').fadeIn('200');
+        $('[data-modal=consultation]').on('click', function () {
+            $('.overlay, #consultation-modal').fadeIn('200');
         });
 
-        $('.button_mini').each(function (i){
-           $(this).on('click', function (){
-               $('#order-modal .modal__descr').text(
-                  $('.catalog-item__subtitle').eq(i).text()
-              );
-               $('.overlay, #order-modal').fadeIn('200');
-           });
+        $('.button_mini').each(function (i) {
+            $(this).on('click', function () {
+                $('#order-modal .modal__descr').text(
+                    $('.catalog-item__subtitle').eq(i).text()
+                );
+                $('.overlay, #order-modal').fadeIn('200');
+            });
         });
 
         closePopUp();
-        $(document).keydown(function (e){
-           if(e.keyCode===27){
-               closeCross();
+        $(document).keydown(function (e) {
+            if (e.keyCode === 27) {
+                closeCross();
+            }
+        });
+
+        function validateForm(item) {
+            $(item).validate({
+                    rules: {
+                        name: {
+                            required: true,
+                            minlength: 2,
+                            maxlength: 12
+                        },
+                        phone: {
+                          required:true
+                        },
+
+                        email: {
+                            required: true,
+                            email: true
+                        }
+                    },
+                messages: {
+                    name: {
+                        required: "Пожалуйста введите ваше имя",
+                        minlength: jQuery.validator.format("Ваше имя должно содержать более {0} символов!")
+                    },
+                    phone:{
+                        required: "Пожалуйста введите ваш номер",
+                    },
+                    email: {
+                        required: "Пожалуйста введите ваш email",
+                        email: "Ваш email должен соответствовать шаблону: ivan@gmail.com!"
+                    }
+                }
+                }
+            );
+        }
+
+        validateForm('#consultation-form');
+        validateForm('#order-modal form');
+        validateForm('#consultation-modal form');
+        $("input[name=phone]").mask("+380 999-999-999");
+
+        //Scroll
+
+        $(window).scroll(function (){
+           if($(this).scrollTop() > 1600){
+               $('.pageup').fadeIn("slow");
+           }else{
+               $('.pageup').fadeOut("slow");
            }
         });
 
-
+        new WOW().init();
     });
